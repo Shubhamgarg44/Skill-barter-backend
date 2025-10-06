05bed3965a2a0db51511e2e3c46e348bbf78f294
shubham garg <118817266+Shubhamgarg44@users.noreply.github.com>
2025-10-06 17:03:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "skill-barter-frontend.vercel.app",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
